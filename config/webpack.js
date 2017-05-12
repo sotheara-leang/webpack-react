f222c6ce8cb87f10704468eaa1c1a03728996e4d
Sotheara Leang <sotheara.leang@mcnc.co.kr>
2017-05-12 17:02:23 +0700

    Commit

--- config/webpack.js
+++ config/webpack.js
@@ -96,12 +96,13 @@ module.exports = {
           return module.context && module.context.indexOf('node_modules') !== -1;
        }
      }),
-     //CommonChunksPlugin will now extract all the common modules from vendor and main bundles
-     new webpack.optimize.CommonsChunkPlugin({
-         name: 'manifest' //But since there are no more common modules between them we end up with just the runtime code included in the manifest file
-     })
+    //CommonChunksPlugin will now extract all the common modules from vendor and main bundles
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest' //But since there are no more common modules between them we end up with just the runtime code included in the manifest file
+    })
   ],
   resolve: {
+    extensions: ['.js', '.jsx', '.scss'],
     alias: {
       Root: path.join(root, 'src/'),
       Components: path.join(root, 'src/components/'),
